3e842f6d9f05cb88cdafedb4319fa8848236841b
Kaique Tavares <62511035+kaiqueq147@users.noreply.github.com>
2023-11-09 16:40:27 -0300

    hour-time

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 import Countdownm from "./countdown.js";
 
-const diasPassados = new Countdownm("07 November 2023 14:20:00 GMT-0300");
+const diasPassados = new Countdownm("07 November 2023 23:45:00 GMT-0300");
 console.log(diasPassados.year);
 
 let month = 0;
